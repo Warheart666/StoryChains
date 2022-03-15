3349d8561c573ec4dc56a0caa599c552a9588c63
warheart <warheart666@yandex.ru>
2022-03-15 23:37:46 +0300

    added front

---
+++ front/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <cytoscape
+            :config="config"
+            ref="cyRef"
+            v-on:cxttapstart="updateNode"
+            v-on:mousedown="addNode"
+    >
+      <cy-element
+              :definition="def"
+              :key="`${def.data.id}`"
+              v-for="def in elements"
+              v-on:mousedown="deleteNode($event, def.data.id)"
+      />
+    </cytoscape>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import config from "./examaple-config";
+  import VueCytoscape from 'vue-cytoscape'
+
+  Vue.use(VueCytoscape)
+
+  const elements = [...config.elements];
+  delete config.elements;
+
+  export default {
+    data() {
+      return {
+        config,
+        elements
+      };
+    },
+    methods: {
+      addNode(event) {
+        console.log(event.target, this.$refs.cyRef.instance);
+        if (event.target === this.$refs.cyRef.instance)
+          console.log("adding node", event.target);
+      },
+      deleteNode(id) {
+        console.log("node clicked", id);
+      },
+      updateNode(event) {
+        console.log("right click node", event);
+      },
+      preConfig(cytoscape) {
+        console.log("calling pre-config", cytoscape);
+      },
+      afterCreated(cy) {
+        // cy: this is the cytoscape instance
+        console.log("after created", cy);
+      }
+    }
+  };
+</script>
